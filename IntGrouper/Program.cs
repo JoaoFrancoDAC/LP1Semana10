b45c647d6f1d5eead867d2a8adf60015e01424fe
JoaoFrancoDAC <jvfdc15@gmail.com>
2025-05-09 17:39:17 +0100

    solution

--- IntGrouper/Program.cs
+++ IntGrouper/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace IntGrouper
 {
@@ -6,7 +9,51 @@ namespace IntGrouper
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("teste branch");
+            if (args.Length == 0)
+            {
+                return;
+            }
+
+            HashSet<int> intersection = null;
+
+            foreach (string fileName in args)
+            {
+                HashSet<int> fileNumbers = new HashSet<int>();
+
+                using (StreamReader reader = new StreamReader(fileName))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (!int.TryParse(line, out int number))
+                        {
+                            throw new FormatException($"Erro no arquivo {fileName}: '{line}' não é um inteiro.");
+                        }
+                        fileNumbers.Add(number);
+                    }
+                }
+
+                if (intersection == null)
+                {
+                    intersection = fileNumbers;
+                }
+                else
+                {
+                    intersection.IntersectWith(fileNumbers);
+                }
+            }
+
+            if (intersection != null)
+            {
+                foreach (int number in intersection.OrderBy(n => n))
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            else
+            {
+                return;
+            }
         }
     }
 }
