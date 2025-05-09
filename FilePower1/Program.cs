eeebad0006fb9980605c0cff0a12f1f39b335998
JoaoFrancoDAC <jvfdc15@gmail.com>
2025-05-09 15:21:19 +0100

    teste

---
+++ FilePower1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FilePower1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please provide a filename as an argument.");
+                return;
+            }
+
+            string fileName = args[0];
+            Queue<string> inputQueue = new Queue<string>();
+
+            Console.WriteLine($"insira as palavras, para parar pressione enter sem nada estar digitado");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                    break;
+
+                inputQueue.Enqueue(input);
+            }
+
+            try
+            {
+                File.WriteAllLines(fileName, inputQueue);
+
+                Console.WriteLine($"salvo no arquivo de texto {fileName}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"erro! {ex.Message}");
+            }
+        }
+    }
+}
