a52ea099e42926c6ad59b6daf51bd67fc7d92bb4
JoaoFrancoDAC <jvfdc15@gmail.com>
2025-05-09 17:04:05 +0100

    teste

--- IntGrouper/Program.cs
+++ IntGrouper/Program.cs
@@ -6,7 +6,7 @@ namespace IntGrouper
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine("teste branch");
         }
     }
 }
